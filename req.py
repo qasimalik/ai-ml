a67d89a67d35145d597142d8b72a0fd6484d7cb8
Qasim Malik <qsmmlk12@gmail.com>
2024-05-20 00:43:30 +0530

    Init

---
+++ req.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import datasets
+import tensorflow as tf
+import keras
+import torch
+import scipy
+import statsmodels.api as sm
+
+print("All libraries imported successfully!")
